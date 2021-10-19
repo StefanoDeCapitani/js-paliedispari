45900d75342a85078edffa2ba581d21eae7c5ecf
Stefano De Capitani <stefano.deca@live.com>
2021-10-19 11:15:51 +0200

    Refactoring

--- Pari e dispari/js/main.js
+++ Pari e dispari/js/main.js
@@ -1,29 +1,74 @@
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let numberInput = parseInt(form.querySelector('input[type="number"]').value);
-  let userOddOrEvenChoice = form.querySelector(":checked").value;
-  generatedNumber = Math.round(Math.random() * 4) + 1;
-  result = numberInput + generatedNumber;
+form.addEventListener("submit", playTheGame);
 
-  let resultOutput = document.querySelector(".result");
-  let winnerOutput = document.querySelector("#winner");
+function playTheGame(e) {
+  e.preventDefault();
+
+  let numberInput = getUserInputNumber();
+  let generatedNumber = generateRandomNumber();
+  result = calculateResult(numberInput, generatedNumber);
+
+  revealAINumber(generatedNumber);
+  displayResult(result);
+  displayWinner(result);
+}
+
+function getUserInputNumber() {
+  let userInput = form.querySelector('input[type="number"]').value;
+  return parseInt(userInput); //A number is needed to return but the type of the input value is string
+}
+
+function generateRandomNumber() {
+  return Math.round(Math.random() * 4) + 1;
+}
+
+function calculateResult(numberInput, generatedNumber) {
+  return numberInput + generatedNumber;
+}
+
+function revealAINumber(generatedNumber) {
   let randomNumber = document.querySelector("#number-random");
   randomNumber.textContent = generatedNumber;
-  resultOutput.textContent = "Risultato: " + result + ". Il vincitore è:";
+}
 
+function displayResult(result) {
+  let resultOutput = document.querySelector(".result");
+  resultOutput.textContent = "Risultato: " + result + ". Il vincitore è:";
+}
+
+function displayWinner(result) {
+  let winnerOutput = document.querySelector("#winner");
+  let isUserTheWinner = getWinOrNot(result);
+  let message = getWinnerMessage(isUserTheWinner);
+  winnerOutput.textContent = message;
+}
+
+function getWinOrNot() {
+  let userOddOrEvenChoice = form.querySelector(":checked").value;
+  let isUserTheWinner;
   if (userOddOrEvenChoice === "even") {
-    if (result % 2 === 0) {
-      winnerOutput.textContent = "Tu";
-    } else {
-      winnerOutput.textContent = "A.I.";
-    }
+    isUserTheWinner = isEven(result);
   } else {
-    if (result % 2 !== 0) {
-      winnerOutput.textContent = "Tu";
-    } else {
-      winnerOutput.textContent = "A.I.";
-    }
+    isUserTheWinner = isOdd(result);
   }
-});
+  return isUserTheWinner;
+}
+
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function isOdd(number) {
+  return number % 2 !== 0;
+}
+
+function getWinnerMessage(isUserTheWinner) {
+  let message;
+  if (isUserTheWinner) {
+    message = "Tu";
+  } else {
+    message = "A.I.";
+  }
+  return message;
+}
